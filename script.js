b13e9ed79576d141934012a776be5b7a3bc1badc
SarikaRamamoorthy <myothermailid001@gmail.com>
2023-10-14 19:53:28 +0530

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const openWindow = document.querySelectorAll('.window');
+// console.log(openWindow);
+const close = document.querySelector('.closebutton');
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
+
+const openModal = function(){
+    popup.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+for(let i=0;i<openWindow.length;i++){
+    openWindow[i].addEventListener('click',openModal);
+}
+
+const closeModal = function(){
+    popup.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+close.addEventListener('click',closeModal);
+
+overlay.addEventListener('click',closeModal);
+
+document.addEventListener('keydown',function(e){
+    if(e.key === 'Escape'&& !(popup.classList.contains('hidden'))){
+        closeModal();
+    }
+})
